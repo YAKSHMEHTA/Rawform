24ae671d0fe91557b99780cd23e8523f6c0a216d
YAKSHMEHTA <yakshvardhansinghmehta@gmail.com>
2026-04-11 08:16:56 +0530

    burl animation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,10 +3,32 @@ import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import { useState, useRef, useEffect } from "react";
 gsap.registerPlugin(ScrollTrigger);
-function App() {
-  const textRef = useRef(null);
 
+function App() {
+  gsap.registerPlugin(ScrollTrigger);
+  const textRef = useRef(null);
+  const [blur,Setblur] = useState(0);
+
+  const updateBlur = (n)=>{  
+    Setblur(n)
+  }
+  let obj = {val:0}
 useEffect(() => {
+
+  gsap.to(obj,{
+    val : 0.6,
+    scrollTrigger:{
+      markers:true,
+      trigger:".hero",
+      start:"top top",
+      end:"center top",
+      scrub:1,
+    },
+    onUpdate:()=>{
+      updateBlur(obj.val);
+    }
+  })
+
   gsap.fromTo(
     textRef.current,
     { backgroundPositionY: "50%" },   // fromVars — start state
@@ -15,7 +37,7 @@ useEffect(() => {
       fontSize:"25rem",
       ease: "none",
       scrollTrigger: {
-        markers: true,
+       // markers: true,
         trigger: ".hero",
         start: "top top",
         end: "center top",
@@ -26,7 +48,7 @@ useEffect(() => {
 }, []);
 
   return (
-    <div className="h-[200vh] relative">
+    <div className="h-[200vh] relative overflow-hidden">
       <div
         className="h-screen hero w-full flex items-center justify-center"
         style={{
@@ -36,7 +58,9 @@ useEffect(() => {
           backgroundAttachment: "fixed",
         }}
       >
-        <div className="blur-div z-2 h-full w-full  top-0 absolute"></div>
+        <div className="blur-div z-2 h-full w-full  top-0 absolute"
+          style={{backgroundColor:`rgba(255, 255, 255, ${blur})`}}
+        ></div>
         <h1
           ref={textRef}
           className="hero-text z-100 font-black opacity-100 tracking-widest uppercase"
