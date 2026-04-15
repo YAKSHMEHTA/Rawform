3402bca66711d78e64867b202fbf7b6e18147795
YAKSHMEHTA <yakshvardhansinghmehta@gmail.com>
2026-04-15 08:22:02 +0530

    images init

--- Frontend/Components/Demo.jsx
+++ Frontend/Components/Demo.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Demo() {
   return (
     <div>
-      <div className="next fixed top-0 left-0 w-full h-screen bg-black flex items-center justify-center text-white text-3xl">
-        Next Section
+      <div className="next flex fixed overflow-hidden top-0 left-0 w-full h-screen bg-black items-center justify-center text-white text-3xl">
+        <img src="" alt="" />
       </div>
     </div>
   )
