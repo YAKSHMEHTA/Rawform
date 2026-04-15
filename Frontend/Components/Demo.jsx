50ef074b692666c6a2fcaac8f8b0c69806bb6583
YAKSHMEHTA <yakshvardhansinghmehta@gmail.com>
2026-04-15 12:22:30 +0530

    Arrow Svg Follow

--- Frontend/Components/Demo.jsx
+++ Frontend/Components/Demo.jsx
@@ -7,15 +7,33 @@ import Lenis from "@studio-freight/lenis";
 gsap.registerPlugin(ScrollTrigger);
 
 function Demo() {
+  const sliderRef = useRef(null);
+  const cRef = useRef(null);
+
   useEffect(() => {
-    gsap.set(".slider-2",{x:-2400})
+    const area = sliderRef.current;
+    const Cimg = cRef.current;
+
+    area.addEventListener("mousemove", (e) => {
+      Cimg.style.display = "block"
+      Cimg.style.left = e.clientX + "px";
+      Cimg.style.top = e.clientY + "px";
+    });
+    area.addEventListener("mouseleave", (e) => {
+      Cimg.style.display = "none"
+
+    });
+  }, []);
+
+  useEffect(() => {
+    gsap.set(".slider-2", { x: -2400 });
     const tl = gsap.timeline({
       scrollTrigger: {
         trigger: ".slider-2",
         start: "bottom bottom",
         end: "bottom 10%",
         scrub: 1.5,
-        markers: true,
+        //        markers: true,
       },
     });
 
@@ -30,47 +48,65 @@ function Demo() {
 
   return (
     <div>
-      <div className="next  flex slider fixed  top-0 left-0 w-full h-screen bg-white items-center justify-center text-white text-3xl">
+      <img
+        ref={cRef}
+        src="/arrow-right.svg"
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100px",
+          height: "20px",
+          opacity:"100%",
+          display:"none",
+          pointerEvents: "none",
+          transform: "translate(-50%, -50%)",
+          zIndex: 9999,
+        }}
+      />
+      <div
+        ref={sliderRef}
+        style={{cursor:"none"}}
+        className="next  flex slider fixed  top-0 left-0 w-full h-screen bg-white items-center justify-center text-white text-3xl"
+      >
         <div className="slider-2 py-2 gap-75   w-full flex">
           <img
-            src="./public/himage-1.webp"
+            src="/himage-1.webp"
             alt=""
             style={{ height: "48rem", width: "42rem" }}
           />
           <img
-            src="./public/himage-2.webp"
+            src="/himage-2.webp"
             alt=""
             style={{ height: "48rem", width: "42rem" }}
           />
           <img
-            src="./public/himage-3.webp"
+            src="/himage-3.webp"
             alt=""
             style={{ height: "48rem", width: "42rem" }}
           />
           <img
-            src="./public/himage3_new_2.jpg"
+            src="/himage3_new_2.jpg"
             alt=""
             style={{ height: "48rem", width: "42rem" }}
           />
           <img
-            src="./public/himage4.jpg"
+            src="/himage4.jpg"
             alt=""
             style={{ height: "48rem", width: "42rem" }}
           />
           <img
-            src="./public/himage6.webp"
+            src="/himage6.webp"
             alt=""
             style={{ height: "48rem", width: "42rem" }}
           />
           <img
-            src="./public/himage51.webp"
+            src="/himage51.webp"
             alt=""
             style={{ height: "48rem", width: "42rem" }}
           />
         </div>
-        
       </div>
-      
     </div>
   );
 }
