3a5b715003b522075b2b39bc0127f62538998f0d
YAKSHMEHTA <yakshvardhansinghmehta@gmail.com>
2026-04-26 12:07:51 +0530

    add text

--- Frontend/Components/Text.jsx
+++ Frontend/Components/Text.jsx
@@ -2,34 +2,67 @@ import React from "react";
 
 function Text() {
   return (
-    <div
-      className="w-full h-screen relative z-50 px-40 py-25 flex flex-col justify-between
-      bg-gray-800 overflow-hidden"
-      style={{
-        zIndex: "99999",
-        backgroundImage: `url(/text-bg.jpg)`,
-        backgroundSize: "cover",
-      }}
-      
-    >
-      <div className="w-full">
-        <p
-          className="text-[#889E9E] max-sm:text-[1.125rem]"
-          style={{ letterSpacing: "15px", wordSpacing: "25px" }}
-        >
-          .make yourself comfortable
-        </p>
-        <p style={{ letterSpacing: "15px", wordSpacing: "25px" }}>
-          trust your body and feel free
-        </p>
-        <p style={{ letterSpacing: "15px", wordSpacing: "25px" }}>
-          in every self manifestation+
-        </p>
+    <div className="h-screen w-full relative   overflow-clip">
+      <div
+        className="trigger relative z-40 h-full  w-full felx flex-col
+            justify-between px-40 py-25 max-xl:px-5
+        "
+      >
+        <div className="w-full leading-none">
+          <p
+            className="heading-3 font-light whitespace-nowrap
+         text-[#889E9E] max-sm:text-[1.125rem] "
+          >
+            .make yourself comfortable
+          </p>
+          <p
+            className="heading-3 font-light whitespace-nowrap
+         text-[#889E9E] max-sm:text-[1.125rem]"
+          >
+            trust your body and feel free
+          </p>
+          <p
+            className="heading-3 font-light whitespace-nowrap
+         text-[#889E9E] max-sm:text-[1.125rem]"
+          >
+            in every self manifestation+
+          </p>
+        </div>
+        <div className="w-full h-full flex flex-col-reverse leading-none text-white text-[8rem] items-end ">
+          <p
+            className="heading-3 font-light whitespace-nowrap
+          max-sm:text-[1.125rem]"
+          >
+            on classical
+          </p>
+          <p
+            className="heading-3 font-light whitespace-nowrap
+          max-sm:text-[1.125rem]"
+          >
+            elegance
+          </p>
+          <p
+            className="heading-3 font-light whitespace-nowrap
+          max-sm:text-[1.125rem]"
+          >
+
+            modern twist
+          </p>
+        </div>
+      </div>
+
+      <div className="absolute top-0 left-0 w-full h-full ">
+        <img
+          src="/text-bg.jpg"
+          alt=""
+          className="absolute top-0 left-0 h-full w-full object-cover "
+          style={{
+            transform: "translate3d(0px, 4.7215%, 0px) scale(1.2, 1.2)",
+          }}
+        />
       </div>
     </div>
   );
 }
-<p class="heading-3 font-light whitespace-nowrap text-[#889E9E] max-sm:text-[1.125rem]">
-  dwasd
-</p>;
+
 export default Text;
