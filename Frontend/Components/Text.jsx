200c9159644a43b733dd49ef212b2cb1f8e24a49
YAKSHMEHTA <yakshvardhansinghmehta@gmail.com>
2026-04-26 11:36:31 +0530

    push

--- Frontend/Components/Text.jsx
+++ Frontend/Components/Text.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import React from "react";
 
 function Text() {
   return (
-    <div className='w-full h-screen relative z-50  bg-gray-800 overflow-hidden'
-    style={{zIndex:"99999",backgroundImage:`url(/text-bg.jpg)`}}>
-        <p style={{letterSpacing: "15px",wordSpacing:"25px"}} >yaksh vardhan mehta</p>
+    <div
+      className="w-full h-screen relative z-50 px-40 py-25 flex flex-col justify-between
+      bg-gray-800 overflow-hidden"
+      style={{
+        zIndex: "99999",
+        backgroundImage: `url(/text-bg.jpg)`,
+        backgroundSize: "cover",
+      }}
+      
+    >
+      <div className="w-full">
+        <p
+          className="text-[#889E9E] max-sm:text-[1.125rem]"
+          style={{ letterSpacing: "15px", wordSpacing: "25px" }}
+        >
+          .make yourself comfortable
+        </p>
+        <p style={{ letterSpacing: "15px", wordSpacing: "25px" }}>
+          trust your body and feel free
+        </p>
+        <p style={{ letterSpacing: "15px", wordSpacing: "25px" }}>
+          in every self manifestation+
+        </p>
+      </div>
     </div>
-  )
+  );
 }
-
-export default Text
+<p class="heading-3 font-light whitespace-nowrap text-[#889E9E] max-sm:text-[1.125rem]">
+  dwasd
+</p>;
+export default Text;
