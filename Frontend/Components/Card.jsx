c0a11c7e2a9fb56f28dca0a481f3423dfed84a0a
YAKSHMEHTA <yakshvardhansinghmehta@gmail.com>
2026-04-30 12:58:40 +0530

    woking on resuable Card component

--- Frontend/Components/Card.jsx
+++ Frontend/Components/Card.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import React,{useRef} from 'react'
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
 
 function Card({
-    classname="",
-    imgSrc,
+  classname="",
+  imgSrc,
+}){
+  const imgRef = useRef(null)
+  function HandelEnter(){
+    gsap.to(imgRef.current,{
+      scale:1,
+      duration:0.45,
+      ease:"power3.out",
+    })
+  }
 
+  function HandelLeave(){
+    gsap.to(imgRef.current,{
+      scale:1.1,
+      duration:0.45,
+      ease:"power3.out",
+    })
+  }
 
-}) {
   return (
     <div className={classname}>
-      <img src={imgSrc} className='object-cover scale-110' alt="" />
+      <img ref={imgRef} src={imgSrc} onMouseLeave={HandelLeave} className='w-full h-full object-cover id scale-110' onMouseOver={HandelEnter} alt="" />
     </div>
   )
 }
--- Frontend/Components/LineUP.jsx
+++ Frontend/Components/LineUP.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Card from './Card'
 
 function LineUP() {
   return (
-    <div className='h-[70vh] relative   bg-cyan-900'>
-      <h2>yaksh</h2>
+    <div className='h-[90vh] gap-2 relative flex p-35  bg-cyan-900'>
+      <Card classname={'h-96 w-80 overflow-clip'} imgSrc={'/text-bg.jpg'}></Card>
+      <Card classname={'h-96 w-80 overflow-clip'} imgSrc={'/hr2.webp'}></Card>
+      <Card classname={'h-96 w-80 overflow-clip'} imgSrc={'/hr1.webp'}></Card>
+      <Card classname={'h-96 w-80 overflow-clip'} imgSrc={'/hr1.webp'}></Card>
     </div>
   )
 }
